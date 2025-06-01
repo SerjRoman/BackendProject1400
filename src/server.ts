38ad8e83cb206df62dc4ed21f73dafa9c1728016
SerjRoman <sergeyroman26@gmail.com>
2025-06-01 14:02:50 +0100

    added limit to json data

--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,11 @@ const app: Express = express();
 const HOST: string = "localhost";
 const PORT: number = 8000;
 
-app.use(express.json());
+app.use(express.json({limit: '20mb'}));
 
 app.use(cors());
 
-app.use("/users", UserRouter);
+app.use("/api/users", UserRouter);
 
 app.use(errorHandlerMiddleware);
 
